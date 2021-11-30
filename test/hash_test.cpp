650aae8e3f0158655a9790a906312e9a3681d2bd
Keith Adams <kmadams@gmail.com>
2021-11-30 12:43:05 -0800

    Still creeping towards utility...

--- test/hash_test.cpp
+++ test/hash_test.cpp
@@ -7,4 +7,15 @@ TEST(HashTest, testHex) {
    for (auto c : pretty) {
       EXPECT_EQ(c, '0');
    }
+
+   memset(hr.bytes, 0xf1, sizeof(hr.bytes));
+   pretty = hr.hex();
+   for (int ii = 0; ii < HASH_LENGTH * 2; ii++) {
+       auto c = pretty[ii];
+       if ((ii % 2) == 0) {
+           EXPECT_EQ(c, 'f');
+           continue;
+       }
+       EXPECT_EQ(c, '1');
+   }
 }
