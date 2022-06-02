c32f57c195001420d37d3af1716a9609844a2a85
Keith Adams <kma@slack-corp.com>
2022-06-02 04:48:46 +0000

    Splitting out blob header.

--- src/blob.cpp
+++ src/blob.cpp
@@ -1,4 +1,42 @@
 #include "blob.hpp"
 
-// This space left intentionally blank
+Blob::Blob(const uint8_t* bts, size_t size)
+    : hash(computeHash(bts, size))
+    , size(size)
+{
+    auto vbytes = std::malloc(size);
+    dbg(blob, 3, "blob alloc (%zd) -> %p\n", size, vbytes);
+    if (vbytes == nullptr) {
+        throw std::bad_alloc();
+    }
+    memcpy(vbytes, bts, size);
+    bytes = (uint8_t*)vbytes;
+}
 
+Blob::Blob(std::string s)
+: Blob((const uint8_t*)&s[0], s.length()) { }
+
+Blob::Blob(const Blob& rhs)
+: Blob(rhs.bytes, rhs.size) { }
+
+Blob::~Blob() {
+    dbg(blob, 3, "blob free (%p) sz %zd\n", bytes, size);
+    std::free((void*)bytes);
+}
+
+bool
+Blob::check() const {
+    HashState hs;
+    return hs(bytes, size) == hash;
+}
+
+std::string
+Blob::substr(int start, size_t len) const {
+    assert(start + len <= size);
+    return std::string((const char*)bytes + start, len);
+}
+
+std::string
+Blob::string() const {
+        return substr(0, size);
+}
--- src/blob.hpp
+++ src/blob.hpp
@@ -20,43 +20,14 @@ struct Blob {
     const size_t size;
     const uint8_t *bytes;
 
-    Blob(const uint8_t* bts, size_t size)
-        : hash(computeHash(bts, size))
-        , size(size)
-        {
-        auto vbytes = std::malloc(size);
-        dbg(blob, 3, "blob alloc (%zd) -> %p\n", size, vbytes);
-        if (vbytes == nullptr) {
-            throw std::bad_alloc();
-        }
-        memcpy(vbytes, bts, size);
-        bytes = (uint8_t*)vbytes;
-    }
-
-    Blob(std::string s)
-    : Blob((const uint8_t*)&s[0], s.length()) { }
-
+    Blob(const uint8_t* bts, size_t size);
+    Blob(std::string s);
     // Need a copy ctor since Blob() has copy semantics
-    Blob(const Blob& rhs)
-    : Blob(rhs.bytes, rhs.size) { }
+    Blob(const Blob& rhs);
+    ~Blob();
+    bool check() const;
 
-    ~Blob() {
-        dbg(blob, 3, "blob free (%p) sz %zd\n", bytes, size);
-        std::free((void*)bytes);
-    }
-
-    bool check() const {
-        HashState hs;
-        return hs(bytes, size) == hash;
-    }
-
-    std::string substr(int start, size_t len) const {
-        assert(start + len <= size);
-        return std::string((const char*)bytes + start, len);
-    }
-
-    std::string string() const {
-        return substr(0, size);
-    }
+    std::string substr(int start, size_t len) const;
+    std::string string() const;
 };
 
