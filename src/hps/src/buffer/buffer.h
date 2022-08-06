4163c2911a80d8dcbf10795cfd0b9c8e5e392a6a
Keith Adams <kma@slack-corp.com>
2022-08-06 00:19:46 +0000

    Get rid of hps.

--- src/hps/src/buffer/buffer.h
+++
@@ -1,9 +0,0 @@
-#ifndef JL2922_HPS_BUFFER_H
-#define JL2922_HPS_BUFFER_H
-
-#include "char_array_input_buffer.h"
-#include "stream_input_buffer.h"
-#include "stream_output_buffer.h"
-#include "string_input_buffer.h"
-#include "string_output_buffer.h"
-#endif
