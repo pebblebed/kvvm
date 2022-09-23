2cbaa4565395b95b58789c0cdd0d2fcafafc8844
Keith Adams <kma@slack-corp.com>
2022-09-23 01:20:59 +0000

    wasm works!

--- test/wasm_test.cpp
+++ test/wasm_test.cpp
@@ -1,8 +1,24 @@
 #include <gtest/gtest.h>
 #include "../src/wasm_runtime.hpp"
 
+std::string
+load_hello_world() {
+    FILE *fp = fopen(
+        "../src/udfs/rust-hello-world/target/wasm32-wasi/debug/hello-world.wasm",
+        "r");
+    assert(fp);
+    fseek(fp, 0, SEEK_END);
+    auto len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    std::string buf;
+    buf.resize(len);
+    fread(&buf[0], len, 1, fp);
+    fclose(fp);
+    return buf;
+}
+
 TEST(Wasm, Exercise){
-    auto runtime = WasmRuntime::load(std::string("123"));
+    auto runtime = WasmRuntime::load(load_hello_world());
     runtime->run(std::string("snark"));
     delete runtime;
 }
